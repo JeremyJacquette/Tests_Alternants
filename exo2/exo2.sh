52e60334a826bbfec7018793a210086388ff9b46
jérémy <jeremy.jqt123@gmail.com>
2023-10-12 12:10:20 +0200

    add_displayInShell_api

--- exo2/exo2.sh
+++ exo2/exo2.sh
@@ -11,11 +11,33 @@ JSON_DATA=$(curl -s "$API_URL")
 ##todo2 : check if the curl command is successful
 # Conditional for verify if the curl is not empty and if it's not empty we can continue the script. 
 if [ -n "$JSON_DATA" ]; then
-    echo "success"
+    # Extract the results from the JSON data iwth jq command
+  RESULTS=$(echo "$JSON_DATA" | jq -c '.results[]')
+##todo3 : Display all the data, one by one, with a loop.
+# Iterate over the variable results and display the data with IFS command (Internal Field Separator) to split the data into lines. I used this documentation : https://www.malekal.com/bash-ifs-utilisation-exemples/
+  while IFS= read -r record; do
+  #the jq -r is used for remove the double quotes from the output 
+    REGION=$(echo "$record" | jq -r '.region')
+    VILLE=$(echo "$record" | jq -r '.ville')
+    ADRESSE=$(echo "$record" | jq -r '.adresse')
+    SP95_PRIX=$(echo "$record" | jq -r '.sp95_prix // "No data"')
+    SP98_PRIX=$(echo "$record" | jq -r '.sp98_prix // "No data"')
+    GAZOLE_PRIX=$(echo "$record" | jq -r '.gazole_prix // "No data"')
+    
+# Display the data into the terminal using echo command
+    echo "Region: $REGION"
+    echo "  Ville: $VILLE"
+    echo "  Adresse: $ADRESSE"
+    echo "  SP95 Prix: $SP95_PRIX"
+    echo "  SP98 Prix: $SP98_PRIX"
+    echo "  Gazole Prix: $GAZOLE_PRIX"
+    #echo for add space into the terminal for lisibility (optional)
+    echo
+# done is used to end the while loop
+  done <<< "$RESULTS"
 else
-    echo " failed"
-    exit 1
+# error message if the curl command is not successful
+  echo "Failed to retrieve data from the API."
 fi
 
-
-
+###todo4 : add algorithm to find the cheapest gas station
